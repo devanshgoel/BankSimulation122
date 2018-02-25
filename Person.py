796e3ba9d32e8087cbd8fd5ad4cc4b342f79c7ba
devanshgoel <devansh.goel1999@gmail.com>
2018-02-24 19:53:42 -0500

    Create Person.py

---
+++ Person.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self, name, birthdate, phonenumber, address):
+        self.name = name
+        self.birthdate = birthdate
+        self.phonenumber = phonenumber
+        self.address = address
+   
