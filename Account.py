722d7e424a4be784742c2a649ce42a40776f1276
Ethan <ejf5367@psu.edu>
2018-02-25 13:17:47 -0500

    Added some more documentation

--- Account.py
+++ Account.py
@@ -1,38 +1,39 @@
 class Account:
+    """ Base account class to define common attributes and methods between savings and checking """
 	def __init__(self, startingBalance=0):
 		"""
         Summary line.
         Initializes the variables for the account class
-        
+
         Parameters
         ----------
         startingBalance: int
 			the starting balance for the account. If the parameter is not passed, the default balance is 0.
         Returns
         -------
-  		Doesn't return anything
-        
+        Doesn't return anything
+
         """
 		self.balance = startingBalance
-		
+
 	def getBalance(self):
 		"""
         Summary line.
         Checks the current balance of the account and returns it.
-        
+
         Parameters
         ----------
-            
+
         Returns
         -------
         int
-        	returns the balance of the account.
-        
+            returns the balance of the account.
+
         """
 		return self.__balance
-	
-	
-	
+
+
+
 	def withdraw(self, amount):
 		if self.__balance >= amount:
 			newBalance = self.__balance - amount
@@ -40,10 +41,10 @@ class Account:
 			return newBalance
 		else:
 			return "Not enough funds, Current Balance: " + self.__balance
-		
+
 	def deposit(self, amount):
 		balance = self.__balance
 		self.__balance = balance+amount
 		return self.__balance
-		
-		
+
+
