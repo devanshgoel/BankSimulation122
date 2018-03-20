f6e9918a6e318f5866221dc1901f0b6821a853f6
Ethan <ejf5367@psu.edu>
2018-03-19 20:18:50 -0400

    fixed a date time issue

--- Savings.py
+++ Savings.py
@@ -8,7 +8,7 @@ class Savings(Account):
     __interestRate = .01 #Intrest rate 
     def __init__(self, startingBalance=0):
         Account.__init__(self, startingBalance) # inistialize with the same vars a account
-        lastInterestUpdate = (datetime.date.day(), datetime.date.month(), datetime.date.year())
+        self.lastInterestUpdate = datetime.date.today()
 
     @classmethod
     def getMinBal(self):
@@ -18,7 +18,7 @@ class Savings(Account):
     def intrest(self): # THis might not be right - we should discuss how to implement this and possible make a setBalance() method for account so we dont have to access hidden variables
         if self.getBalance() >= 0:
             self._Account__balance *= (1+self.__interestRate)
-            lastIntrestUpdate = datetime.date.today()
+            self.lastIntrestUpdate = datetime.date.today()
             return True
         else:
             print("You have a negative or zero balance - Interest not applied")
@@ -30,8 +30,8 @@ class Savings(Account):
             print("Forcefully updating intrest regardless of date")
             intrest()
         else:
-            now = (datetime.date.day(), datetime.date.month(), datetime.date.year())
-            if now[0] > lastInterestUpdate[0]: # make this work better - check for month/year changes
+            now = datetime.date.today()
+            if now.day > self.lastInterestUpdate.day: # make this work better - check for month/year changes
                 print("Updating intrest")
 
 
